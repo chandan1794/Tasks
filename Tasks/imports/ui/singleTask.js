0b3141ad5b2cf5bfc0a7dd8c854da5739ae68bc7
Chandan Purbia <chandanpurbiawork@gmail.com>
2017-08-20 23:56:17 +0530

    Seperated the singleTask template to singleTaskBy and singleTaskTo.

--- Tasks/imports/ui/singleTask.js
+++ Tasks/imports/ui/singleTask.js
@@ -1,35 +1,50 @@
 import { Template } from 'meteor/templating';
 import './singleTask.html';
 import { TASK_STATUS } from '../api/constants.js';
+import { MapStatus, ConstructDate } from '../api/helperFunctions.js';
 
-Template.singleTask.helpers({
+Template.singleTaskTo.helpers({
     GetStatus: function(status){
-        switch(status){
-            case TASK_STATUS["REASSIGNED"]:
-            case TASK_STATUS["PENDING"]: return "warning";
-            case TASK_STATUS["SUBMITTED"]: return "info";
-            case TASK_STATUS["APPROVED"]: return "success";
-        }
+        return MapStatus(status, TASK_STATUS);
     },
 
     // function to format date
     ExtractDate: function(date){
-        var fullDate = "";
+        return ConstructDate(date);
+    },
 
-        var temp = date.getDate();
-        if(temp<10){
-            fullDate+="0";
-        }
-        fullDate+=temp;
-        fullDate+="-";
+    IsTaskPending: function(status){
+        return TASK_STATUS["PENDING"] === status || TASK_STATUS["REASSIGNED"] === status;
+    },
 
-        temp = date.getMonth();
-        if(temp<10){
-            fullDate+="0";
-        }
-        fullDate+=temp;
-        fullDate+="-";
+    IsTaskSubmitted: function(status){
+        return TASK_STATUS["SUBMITTED"] === status;
+    },
 
-        return (fullDate+date.getFullYear());
-    }
+    IsTaskApproved: function(status){
+        return TASK_STATUS["APPROVED"] === status;
+    },
+})
+
+Template.singleTaskBy.helpers({
+    GetStatus: function(status){
+        return MapStatus(status, TASK_STATUS);
+    },
+
+    // function to format date
+    ExtractDate: function(date){
+        return ConstructDate(date);
+    },
+
+    IsTaskSubmitted: function(status){
+        return TASK_STATUS["SUBMITTED"] === status;
+    },
+
+    IsTaskApproved: function(status){
+        return TASK_STATUS["APPROVED"] === status;
+    },
+
+    IsTaskPending: function(status){
+        return TASK_STATUS["PENDING"] === status || TASK_STATUS["REASSIGNED"] === status;
+    },
 })
