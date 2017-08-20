f47791be4ce048af5777829d83670405fcb00618
Chandan Purbia <chandanpurbiawork@gmail.com>
2017-08-20 22:55:36 +0530

    Formatted date to dd-mm-yyyy.

--- Tasks/imports/ui/singleTask.js
+++ Tasks/imports/ui/singleTask.js
@@ -10,5 +10,26 @@ Template.singleTask.helpers({
             case TASK_STATUS["SUBMITTED"]: return "info";
             case TASK_STATUS["APPROVED"]: return "success";
         }
+    },
+
+    // function to format date
+    ExtractDate: function(date){
+        var fullDate = "";
+
+        var temp = date.getDate();
+        if(temp<10){
+            fullDate+="0";
+        }
+        fullDate+=temp;
+        fullDate+="-";
+
+        temp = date.getMonth();
+        if(temp<10){
+            fullDate+="0";
+        }
+        fullDate+=temp;
+        fullDate+="-";
+
+        return (fullDate+date.getFullYear());
     }
 })
