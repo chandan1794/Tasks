c4e6bff8d975c1054f4179eaec8394211b648ec3
Chandan Purbia <chandanpurbiawork@gmail.com>
2017-08-20 22:51:01 +0530

    Made AllTasks() return both results 	- assignBy current user 	- assigneTo current user

--- Tasks/imports/ui/allTasks.js
+++ Tasks/imports/ui/allTasks.js
@@ -2,9 +2,13 @@ import { Template } from 'meteor/templating';
 import './allTasks.html';
 import './singleTask.js';
 import { tasks } from '../api/collections.js';
+import { USER_ROLE } from '../api/constants.js'
 
 Template.allTasks.helpers({
-    AllTasks: function(){
-        return tasks.find({"assignedTo":Meteor.user().username}, { "sort": { "dateOfAssignment" :-1 }});
+    AllTasks: function(role){
+        if(role === USER_ROLE["RECIEVER"])
+            return tasks.find({"assignedTo":Meteor.user().username}, { "sort": { "dateOfAssignment" :-1 }});
+        else
+            return tasks.find({"assignedBy":Meteor.user().username}, { "sort": { "dateOfAssignment" :-1 }});
     }
 })
