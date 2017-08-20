cd058961f5fcb5014f478a48cf5b4d709fde66d5
Chandan Purbia <chandanpurbiawork@gmail.com>
2017-08-20 18:20:22 +0530

    Implemented function to add the form details to database.

--- Tasks/imports/ui/addATask.js
+++ Tasks/imports/ui/addATask.js
@@ -1,2 +1,27 @@
 import { Template } from 'meteor/templating';
+import { tasks } from '../api/collections.js';
+import { TASK_STATUS } from '../api/constants.js';
 import './addATask.html';
+
+Template.addATask.events({
+    'submit #addATaskForm' : function(event){
+        event.preventDefault();
+        tasks.insert({
+            "task": $("#formTask").val(),
+            "assignedBy": Meteor.user().username,
+            "assignedTo": $("#formAssignedTo").val(),
+            "dateOfAssignment": new Date(),
+            "dateOfCompletion": null,
+            "status": TASK_STATUS["PENDING"],
+        }, function(error){
+            if(error){
+                $('#formSubmitError').css('display','block');
+                return false;
+            }else{
+                $('#formSubmitSuccess').css('display','block');
+                document.getElementById("addATaskForm").reset(); //Resetting form to initial values
+                return false;
+            }
+        });
+    },
+});
