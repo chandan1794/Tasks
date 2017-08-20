d562f83fdde4b6edf4b71d191e451d0f1645cd23
Chandan Purbia <chandanpurbiawork@gmail.com>
2017-08-20 21:57:23 +0530

    Added color according to status of the task.

--- Tasks/imports/ui/singleTask.js
+++ Tasks/imports/ui/singleTask.js
@@ -1,2 +1,14 @@
 import { Template } from 'meteor/templating';
 import './singleTask.html';
+import { TASK_STATUS } from '../api/constants.js';
+
+Template.singleTask.helpers({
+    GetStatus: function(status){
+        switch(status){
+            case TASK_STATUS["REASSIGNED"]:
+            case TASK_STATUS["PENDING"]: return "warning";
+            case TASK_STATUS["SUBMITTED"]: return "info";
+            case TASK_STATUS["APPROVED"]: return "success";
+        }
+    }
+})
