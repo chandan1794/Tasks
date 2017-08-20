7c9f3fddf0bd9d9df3c10506d8537bf42bad1406
Chandan Purbia <chandanpurbiawork@gmail.com>
2017-08-20 18:33:39 +0530

    Implemented showing all tasks that been added to the database.

--- Tasks/imports/ui/allTasks.js
+++ Tasks/imports/ui/allTasks.js
@@ -1,3 +1,10 @@
 import { Template } from 'meteor/templating';
 import './allTasks.html';
 import './singleTask.js';
+import { tasks } from '../api/collections.js';
+
+Template.allTasks.helpers({
+    AllTasks: function(){
+        return tasks.find({}, { "sort": { "dateOfAssignment" :-1 }});
+    }
+})
