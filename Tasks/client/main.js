35ef64a7d85f4e9ab2e5f5ea4aab5052d5970c89
Chandan Purbia <chandanpurbiawork@gmail.com>
2017-08-20 18:20:52 +0530

    Imported collections.js to both client and server side.

--- Tasks/client/main.js
+++ Tasks/client/main.js
@@ -1,4 +1,5 @@
 import { Template } from 'meteor/templating';
 import '../imports/startup/accounts-config.js';
 
+import '../imports/api/collections.js';
 import '../imports/ui/body.js';
--- Tasks/server/main.js
+++ Tasks/server/main.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
-
+import '../imports/api/collections.js'; // Import collection on both server and client side else
+// it will give error.
 Meteor.startup(() => {
   // code to run on server at startup
 });
