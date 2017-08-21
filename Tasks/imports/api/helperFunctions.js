602a527c56cb087c77fd927ff5f3171787fe8eeb
Chandan Purbia <chandanpurbiawork@gmail.com>
2017-08-22 00:46:17 +0530

    Now the tasks are sorted by a customized priority given in the code. Function is designed is such a way that you can decide whatever priority you want.

--- Tasks/imports/api/helperFunctions.js
+++ Tasks/imports/api/helperFunctions.js
@@ -1,3 +1,5 @@
+import { TASK_STATUS } from './constants.js'
+
 export const MapStatus = function(status, TASK_STATUS){
     switch(status){
         case TASK_STATUS["REASSIGNED"]:
@@ -23,4 +25,39 @@ export const ConstructDate = function(date){
     fullDate+="-";
 
     return (fullDate+date.getFullYear());
+}
+
+
+// This function is sorting tasks according to the status first and then time of assignment
+// Status priority is now fixed. Will give soon an option to customize.
+
+// This function has a flaw that it is using 2 times more space.
+// Space optimization can be an issue in future when a sinnlge user of thousands of tasks.
+export const SortAccordingToStatus = function(tasks, order){
+    var pending = [];
+    var submitted = [];
+    var accepted = [];
+    tasks.forEach(function(task){
+        if(task.status === TASK_STATUS["PENDING"] || task.status === TASK_STATUS["REASSIGNED"])
+            pending.push(task);
+        else{
+            if(task.status === TASK_STATUS["SUBMITTED"])
+                submitted.push(task);
+            else
+                accepted.push(task);
+        }
+    });
+    // Arraging array of tasks according to custmized order given by user.
+    var finalOrder = [];
+    order.forEach(function(status){
+        if(TASK_STATUS["PENDING"] === status)
+            finalOrder = finalOrder.concat(pending);
+        else{
+            if(TASK_STATUS["SUBMITTED"] === status)
+                finalOrder = finalOrder.concat(submitted);
+            else
+                finalOrder = finalOrder.concat(accepted);
+        }
+    })
+    return finalOrder;
 }
--- Tasks/imports/ui/allTasks.js
+++ Tasks/imports/ui/allTasks.js
@@ -2,13 +2,18 @@ import { Template } from 'meteor/templating';
 import './allTasks.html';
 import './singleTask.js';
 import { tasks } from '../api/collections.js';
-import { USER_ROLE } from '../api/constants.js'
+import { USER_ROLE, TASK_STATUS } from '../api/constants.js'
+import { SortAccordingToStatus } from '../api/helperFunctions.js'
 
 Template.allTasks.helpers({
     AllTasks: function(role){
-        if(role === USER_ROLE["RECIEVER"])
-            return tasks.find({"assignedTo":Meteor.user().username}, { "sort": { "dateOfAssignment" :-1 }});
-        else
-            return tasks.find({"assignedBy":Meteor.user().username}, { "sort": { "dateOfAssignment" :-1 }});
+        if(role === USER_ROLE["RECIEVER"]){
+            var tasksArray =  tasks.find({"assignedTo":Meteor.user().username}, { "sort": { "dateOfAssignment" :-1 }});
+            return SortAccordingToStatus(tasksArray, [ TASK_STATUS["PENDING"], TASK_STATUS["SUBMITTED"], TASK_STATUS["APPROVED"] ]);
+        }
+        else{
+            var tasksArray =  tasks.find({"assignedBy":Meteor.user().username}, { "sort": { "dateOfAssignment" :-1 }});
+            return SortAccordingToStatus(tasksArray, [ TASK_STATUS["SUBMITTED"], TASK_STATUS["PENDING"], TASK_STATUS["APPROVED"] ]);
+        }
     }
 })
