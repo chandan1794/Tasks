d44c040e1b33d1cf0f7c925a1af15153c130cbef
Chandan Purbia <chandanpurbiawork@gmail.com>
2017-08-21 23:54:08 +0530

    Added signing out facility.

--- Tasks/imports/ui/navigationBar.js
+++ Tasks/imports/ui/navigationBar.js
@@ -1,1 +1,13 @@
 import './navigationBar.html';
+
+Template.navigationBar.events({
+    'click #signOut': function(event){
+        Meteor.logout(function(error){
+            if(error){
+                alert("Error in logging out!!");
+            }else{
+                alert("Succefully Logged Out!!");
+            }
+        })
+    }
+})
